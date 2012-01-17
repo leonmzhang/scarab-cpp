7b022d132f0e95ec5e3650b8f1ab70ca0f43cc20
mingzhang2 <mingzhang2@iflytek.com>
2012-01-17 15:34:03 +0800

    m

--- src/base64/sc_base64.cpp
+++ src/base64/sc_base64.cpp
@@ -36,21 +36,20 @@ int base64_encode(const char *ptr, int ptr_len, char *buf, int buf_len)
   
   if(remainder != 0)
   {
-    char buf[3];
-    memset(buf, 0, 3);
-    memcpy(buf, p_src, remainder);
-    *p_des       = base64_encode_map[(*buf >> 2) & 0x3f];
-    *(p_des + 1) = base64_encode_map[((*buf << 4) & 0x30) | ((*(buf + 1) >> 4) & 0x0f)];
-    *(p_des + 2) = base64_encode_map[(*(buf + 1) << 2 & 0x3c) | ((*(buf + 2) >> 6) & 0x03)];
-    *(p_des + 3) = base64_encode_map[*(buf + 2) & 0x3f];
+    char inner_buf[3];
+    memset(inner_buf, 0, 3);
+    memcpy(inner_buf, p_src, remainder);
+    *p_des       = base64_encode_map[(*inner_buf >> 2) & 0x3f];
+    *(p_des + 1) = base64_encode_map[((*inner_buf << 4) & 0x30) | ((*(inner_buf + 1) >> 4) & 0x0f)];
+    *(p_des + 2) = base64_encode_map[(*(inner_buf + 1) << 2 & 0x3c) | ((*(inner_buf + 2) >> 6) & 0x03)];
+    *(p_des + 3) = base64_encode_map[*(inner_buf + 2) & 0x3f];
     p_src += remainder;
     p_des += 4;
-  }
-
-  p_des = p_des - (3 - remainder);
-  for(int i = 0; i < (3 - remainder); i++)
-  {
-    *p_des++ = '=';
+	p_des = p_des - (3 - remainder);
+	for(int i = 0; i < (3 - remainder); i++)
+	{
+		*p_des++ = '=';
+	}
   }
 
   return p_des - buf;
