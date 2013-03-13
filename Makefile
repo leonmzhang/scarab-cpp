be718e32e42821fbc8f2564f191f8fc4f8fa069a
Leon Zhang <leonmzhang@gmail.com>
2013-03-13 23:08:21 +0800

    m

--- Makefile
+++ Makefile
@@ -1,1 +1,10 @@
 # makefile
+
+test : test.o
+	g++ -o test test.o
+
+test.o : test.cpp include/sc_utils.h
+	g++ -o test.o -c -g test.cpp
+
+clean :
+	rm *.o test
--- test.cpp
+++ test.cpp
@@ -1,1 +1,19 @@
 #include "include/sc_utils.h"
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+  //  char * str = "\tthis is a test\t";
+  char * str = "";
+  char * pstr = new char[1024];
+  memset(pstr, 0, 1024);
+  memcpy(pstr, str, strlen(str) + 1);
+  trim_str(pstr, 't');
+
+  cout << "#" << pstr << "#" << endl;
+  cout << strlen(pstr) << endl;
+
+  return 0;
+}
