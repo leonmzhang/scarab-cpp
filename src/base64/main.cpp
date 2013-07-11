bf404a744b7d96ffa16cd37a4f79b75ab0454d94
mingzhang2 <mingzhang2@iflytek.com>
2013-07-11 09:58:57 +0800

    m

--- src/base64/main.cpp
+++ src/base64/main.cpp
@@ -7,9 +7,14 @@ int main(int argc, char *argv[])
   char *ptr = "this is a test";
   char *buf = new char[1024];
   memset(buf, 0, 1024);
-  
   int len = base64_encode(ptr, strlen(ptr), buf, 1024);
 
-  printf("%s\t%d\n", buf, len);
+  char *ptr2 = "dGhpcyBpcyBhIHRlc3Q=";
+  char *buf2 = new char[1024];
+  memset(buf2, 0, 1024);
+  int len2 = base64_decode(ptr2, strlen(ptr2), buf2, 1024);
+
+  printf("%s\t%d\n", buf2, len2);
   delete[] buf;
+  delete[] buf2;
 }
--- src/base64/sc_base64.cpp
+++ src/base64/sc_base64.cpp
@@ -1,10 +1,20 @@
 #include "sc_base64.h"
 #include <string.h>
+#include <stdio.h>
 
 int base64_encode(const char *ptr, int ptr_len, char *buf, int buf_len)
 {
   int encode_len = ptr_len / 3 * 4 + (ptr_len % 3 == 0 ? 0 : 4);
-  if(encode_len > buf_len)
+  if(ptr == NULL)
+  {
+    return -1;
+  }
+
+  if(buf == NULL)
+  {
+    return encode_len;
+  }
+  else if(encode_len > buf_len)
   {
     return -1;
   }
@@ -16,10 +26,10 @@ int base64_encode(const char *ptr, int ptr_len, char *buf, int buf_len)
   
   for(int i = 0; i < loop_count; i++)
   {
-    *p_des       = base64_map[(*p_src >> 2) & 0x3f];
-    *(p_des + 1) = base64_map[((*p_src << 4) & 0x30) | ((*(p_src + 1) >> 4) & 0x0f)];
-    *(p_des + 2) = base64_map[(*(p_src + 1) << 2 & 0x3c) | ((*(p_src + 2) >> 6) & 0x03)];
-    *(p_des + 3) = base64_map[*(p_src + 2) & 0x3f];
+    *p_des       = base64_encode_map[(*p_src >> 2) & 0x3f];
+    *(p_des + 1) = base64_encode_map[((*p_src << 4) & 0x30) | ((*(p_src + 1) >> 4) & 0x0f)];
+    *(p_des + 2) = base64_encode_map[(*(p_src + 1) << 2 & 0x3c) | ((*(p_src + 2) >> 6) & 0x03)];
+    *(p_des + 3) = base64_encode_map[*(p_src + 2) & 0x3f];
     p_src += 3;
     p_des += 4;
   }
@@ -29,10 +39,10 @@ int base64_encode(const char *ptr, int ptr_len, char *buf, int buf_len)
     char buf[3];
     memset(buf, 0, 3);
     memcpy(buf, p_src, remainder);
-    *p_des       = base64_map[(*buf >> 2) & 0x3f];
-    *(p_des + 1) = base64_map[((*buf << 4) & 0x30) | ((*(buf + 1) >> 4) & 0x0f)];
-    *(p_des + 2) = base64_map[(*(buf + 1) << 2 & 0x3c) | ((*(buf + 2) >> 6) & 0x03)];
-    *(p_des + 3) = base64_map[*(buf + 2) & 0x3f];
+    *p_des       = base64_encode_map[(*buf >> 2) & 0x3f];
+    *(p_des + 1) = base64_encode_map[((*buf << 4) & 0x30) | ((*(buf + 1) >> 4) & 0x0f)];
+    *(p_des + 2) = base64_encode_map[(*(buf + 1) << 2 & 0x3c) | ((*(buf + 2) >> 6) & 0x03)];
+    *(p_des + 3) = base64_encode_map[*(buf + 2) & 0x3f];
     p_src += remainder;
     p_des += 4;
   }
@@ -49,11 +59,52 @@ int base64_encode(const char *ptr, int ptr_len, char *buf, int buf_len)
 int base64_decode(const char *ptr, int ptr_len, char *buf, int buf_len)
 {
   int remainder = 0;
-  char *p_src = ptr + ptr_len;
+  const char *p_src = ptr + ptr_len - 1;
   while(*p_src == '=')
   {
-    
+    p_src--;
   }
-  
-  int decode_len = ptr_len * 4 / 3;
+  remainder = ptr_len - 1 - (p_src - ptr);
+  int decode_len = (((ptr_len - 4) / 4 * 3) + (3 - remainder));
+  if(decode_len > buf_len)
+  {
+    return -1;
+  }
+  if(ptr_len % 4 != 0)
+  {
+    return -1;
+  }
+
+  p_src = ptr;
+  char *p_des = buf;
+  int loop_count = ptr_len / 4 - 1;
+  for(int i = 0; i < loop_count; i++)
+  {
+    *p_des = ((base64_decode_map[*p_src - 43] << 2) & 0xfc) 
+      | ((base64_decode_map[*(p_src + 1) - 43] >> 4) & 0x03);
+    *(p_des + 1) = ((base64_decode_map[*(p_src + 1) - 43] << 4) & 0xf0)
+      | ((base64_decode_map[*(p_src + 2) - 43] >> 2) & 0x0f);
+    *(p_des + 2) = ((base64_decode_map[*(p_src + 2) - 43] << 6) & 0xc0)
+      | (base64_decode_map[*(p_src + 3) - 43] & 0x3f);
+    p_src += 4;
+    p_des += 3;
+  }
+
+  char inner_buf[4];
+  memcpy(inner_buf, p_src, 4);
+  for(int i = 0; i < remainder; i++)
+  {
+    inner_buf[4 - i - 1] = '\0';
+  }
+
+  *p_des = ((base64_decode_map[inner_buf[0] - 43] << 2) & 0xfc) 
+    | ((base64_decode_map[inner_buf[1] - 43] >> 4) & 0x03);
+  *(p_des + 1) = ((base64_decode_map[inner_buf[1] - 43] << 4) & 0xf0) 
+    | ((base64_decode_map[inner_buf[2] - 43] >> 2) & 0x0f);
+  *(p_des + 2) = ((base64_decode_map[inner_buf[2] - 43] << 6) & 0xc0) 
+    | (base64_decode_map[inner_buf[3] - 43] & 0x3f);
+  p_src += 4;
+  p_des += 3 - remainder;
+
+  return p_des - buf;
 }
